f2068924e17eef6f96c24c331a1c125598b49c46
Ahmad Ansori <59714486+ansoriweb@users.noreply.github.com>
2020-03-12 08:52:40 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Tentang Saya | Buat Surat Izin Sekolah Generator</title>
+  <link rel="icon" type="image/png" href="img/icon.png">
+      <meta name="description" content="Buat Surat Izin Sekolah Kamu disini dijamin Cepat dan Mudah">
+  <meta name="keywords" content="Bikin SUrat,surat,surat izin,make surat izin,surat sekolah,contoh surat sekolah,surat sekolah generator,surat izin generator,surat izin tidak masuk sekolah,surat sekolah,surat tdk masuk,surat izin tdk sekolah">
+  <meta name="author" content="Ahmad Ansori">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <style>
+  body {
+    font: 20px Montserrat, sans-serif;
+    line-height: 1.8;
+    color: black;
+  }
+  p {font-size: 16px;}
+  .margin {margin-bottom: 45px;}
+  .bg-1 { 
+    background-color: #1abc9c; /* Green */
+    color: #ffffff;
+  }
+  .bg-2 { 
+    background-color: #474e5d; /* Dark Blue */
+    color: #ffffff;
+  }
+  .bg-3 { 
+    background-color: #ffffff; /* White */
+    color: #555555;
+  }
+  .bg-4 { 
+    background-color: #2f2f2f; /* Black Gray */
+    color: #fff;
+  }
+  .container-fluid {
+    padding-top: 70px;
+    padding-bottom: 70px;
+  }
+  .navbar {
+    padding-top: 15px;
+    padding-bottom: 15px;
+    border: 0;
+    border-radius: 0;
+    margin-bottom: 0;
+    font-size: 12px;
+    letter-spacing: 5px;
+  }
+  .navbar-nav  li a:hover {
+    color: #1abc9c !important;
+  }
+  </style>
+</head>
+<body>
+<!-- Navbar -->
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="/">Home</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="tutorial.php">Tutorial</a></li>
+        <li><a href="about.php">Tentang Saya</a></li>
+        <li><a href="contact.php">Kontak</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<center>
+<h2>Tentang Saya</h2><br>
+<img src="img/profile.jpg" class="img-rounded" height="300px">
+<h3>Nama: Ahmad Ansori<br>
+TTL: Nganjuk, 01 Juni 2019<br>
+Sekolah: SMKN 1 KERTOSONO<br>
+Hobby: Ngoding<br>
+Cita-Cita: Progammer</center></h3><br>
+
+<!-- Footer -->
+<footer class="container-fluid bg-4 text-center">
+  <p>Aplikasi Web dibuat oleh <a href="http://www.ahmadansori.xyz">@ahmadansori_id</a></p> 
+</footer>
+</body>
+</html>
